50b3af335d88fb2b8a594c215fe42848e69383bf
cong34dk <nguyencong12g2003@gmail.com>
2024-07-29 16:58:51 +0700

    Initial commit - backend

---
+++ backend/Data/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
